b718141143ea775f297fdb3c6f372588cc4047e5
LemiSt24 <lennard.strohmeyer@gmail.com>
2018-09-05 14:25:18 +0200

    implement basic passthrough functionality

--- circuit.py
+++ circuit.py
@@ -4,6 +4,9 @@ import sys
 
 midiin = rtmidi.RtMidiIn()
 
+circuit = None
+bcf2000 = None
+
 def print_message(midi):
 	if midi.isNoteOn():
 		print("ON: ", midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
@@ -33,10 +36,12 @@ class CircuitController(threading.Thread):
 	def handleMessage(self, msg):
 		if msg.isController(): #ignoring note and timing messages for now
 			if self.passthrough:
-				#send message to BCF2000
+				bcf2000.output.sendMessage(msg)	#send message to BCF2000
 		elif msg.isSysEx():
 			patchData = self.unpackPatch(msg)
 			#send patch data to BCF2000
+	def unpackPatch(self, msg):
+		return None
 
 	def run(self):
 		while True:
@@ -44,21 +49,58 @@ class CircuitController(threading.Thread):
 				return
 			msg = self.input.getMessage() #listens for messages
 			if msg:
-				print_message(msg)
+				self.handleMessage(msg)
+				#print_message(msg)
+
+class BCFController(threading.Thread):
+	def __init__(self, port):
+		threading.Thread.__init__(self) #threading
+		self.setDaemon(True) #threading
+		self.port = port #the internal midi port
+		self.input = None #object for rtmidiin
+		self.output = None #object for rtmidiout
+		self.passthrough = False #enable if the two devices are not connected outside of the computer running this software
+		self.quit = False #threading
+
+	def connect(self): #open midi port for sending and receiving data
+		self.input = rtmidi.RtMidiIn()
+		self.input.openPort(self.port)
+		self.output = rtmidi.RtMidiOut()
+		self.output.openPort(self.port)
+
+	def handleMessage(self, msg):
+		if msg.isController(): #ignoring note and timing messages for now
+			if self.passthrough:
+				circuit.output.sendMessage(msg) #send message to circuit
+
+	def run(self):
+		while True:
+			if self.quit:
+				return
+			msg = self.input.getMessage() #listens for messages
+			if msg:
+				self.handleMessage(msg)
+				#print_message(msg)
 
 
 ports = range(midiin.getPortCount())
 if ports:
-	selected = 0
-	while selected == 0:
+	circuitport = 0
+	bcfport = 0
+	while circuitport == 0 and bcfport == 0:
 		for i in ports:
 			if "Circuit" in midiin.getPortName(i):
-				selected = i
-	print("Oeffne Port ", selected)
-	circuit = CircuitController(selected)
+				circuitport = i
+			if "BCF2000" in midiin.getPortName(i):
+				bcfport = i
+
+	circuit = CircuitController(circuitport)
 	circuit.connect()
 	circuit.start()
 
+	bcf2000 = BCFController(bcfport)
+	bcf2000.connect()
+	bcf2000.start()
 
 	sys.stdin.read(1)
 	circuit.quit = True
